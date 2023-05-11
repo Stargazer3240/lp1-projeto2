2e776b443f439b4c26674115c6912b503423960c
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-05-11 13:07:14 -0300

    Made the musics and playlists in made dynamic allocated

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,29 @@ int main() {
   List<Playlist*> playlists;
   List<Music*> musics;
 
-  Playlist play1("As 100 Melhores do Samba");
-  Playlist play2("Alternative Metal");
-  Music mus1("O mundo é um moinho", "Cartola");
-  Music mus2("Saudosa Maloca", "Adoniran Barbosa");
-  Music mus3("Toxicity", "System of a Down");
-  Music mus4("Aerials", "System of a Down");
-  Music mus5("Dead Memories", "Slipknot");
+  auto play1 = new Playlist("As 100 Melhores do Samba");
+  auto play2 = new Playlist("Alternative Metal");
+  auto mus1 = new Music("O mundo é um moinho", "Cartola");
+  auto mus2 = new Music("Saudosa Maloca", "Adoniran Barbosa");
+  auto mus3 = new Music("Toxicity", "System of a Down");
+  auto mus4 = new Music("Aerials", "System of a Down");
+  auto mus5 = new Music("Dead Memories", "Slipknot");
 
-  playlists.push_back(&play1);
-  playlists.push_back(&play2);
-  musics.push_back(&mus1);
-  musics.push_back(&mus2);
-  musics.push_back(&mus3);
-  musics.push_back(&mus4);
-  musics.push_back(&mus5);
+  playlists.push_back(play1);
+  playlists.push_back(play2);
+  musics.push_back(mus1);
+  musics.push_back(mus2);
+  musics.push_back(mus3);
+  musics.push_back(mus4);
+  musics.push_back(mus5);
 
-  playlists.get(0)->value->add_music(&mus1);
-  playlists.get(0)->value->add_music(&mus4);
-  playlists.get(0)->value->add_music(&mus2);
-  playlists.get(1)->value->add_music(&mus3);
-  playlists.get(1)->value->add_music(&mus4);
-  playlists.get(1)->value->add_music(&mus5);
-  playlists.get(1)->value->add_music(&mus2);
+  playlists.get(0)->value->add_music(mus1);
+  playlists.get(0)->value->add_music(mus4);
+  playlists.get(0)->value->add_music(mus2);
+  playlists.get(1)->value->add_music(mus3);
+  playlists.get(1)->value->add_music(mus4);
+  playlists.get(1)->value->add_music(mus5);
+  playlists.get(1)->value->add_music(mus2);
 
   // User Interface.
   while (main_menu(playlists, musics)) {
